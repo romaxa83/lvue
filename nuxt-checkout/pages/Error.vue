96f70d8363a76e58e04aeaf71293da839c486197
roman <romaxa83@ukr.net>
2021-08-12 21:56:56 +0300

    nuxt-checkout

---
+++ nuxt-checkout/pages/Error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="py-5 text-center">
+      <h2>Error</h2>
+      <p class="lead">Couldn't process payment!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error"
+}
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ nuxt-checkout/pages/Success.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="py-5 text-center">
+      <h2>Success</h2>
+      <p class="lead">You purchase has been completed!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Success"
+}
+</script>
+
+<style scoped>
+
+</style>
